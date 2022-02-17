87e1f0b3df1108dbfdfec3b06a9c39cc5649d4fc
rodrigosalazarvega <20171406@aloe.ulima.edu.pe>
2022-02-17 14:53:37 -0500

    CompraBtc1,2,3. TYC y Nosotros

--- cryptonita/cryptonita_ulima/pages/nosotros/index.js
+++ cryptonita/cryptonita_ulima/pages/nosotros/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
+import Footer from "../components/footer.component"
+import Header from "../components/header.component"
+import React, { useEffect } from "react";
 
 export default function Home() {
   return (
@@ -16,11 +16,7 @@ export default function Home() {
 
       
       <main className={styles.main}>
-      <header className={styles.header}>
-        
-        <Image src="/meta.png" alt="Vercel Logo" width={150} height={70} />
-            
-          </header>
+
       <header className={styles.back}>
         
         <Image src="/back.png" alt="Vercel Logo" width={40} height={40} />
@@ -73,55 +69,7 @@ export default function Home() {
           
           
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-      <div class="row">
-    <div class="col">
-    <span className={styles.logo}>
-            <Image src="/logo.jpg" alt="Vercel Logo" width={1000} height={500} />
-          </span>
-    </div>
-    <div class="col-6">
-    <div class="row">
-  <div class="col-4">
-      <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <p>Nosotros
-
-            </p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <p>T Y C</p>
-          </a>
-
-          
-        </div></div>
-  <div class="col-8">
-  <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <p>2022 Nombre todos los derechos reservados
-            </p>
-          </a>
-
-         
-
-          
-        </div></div>
-</div>
-
-    </div>
-    <div class="col">
-      <p>Rodrigo Salazar Vega</p>
-      <p>Luis Manuel Mondoñedo Cabanillas - 20173617</p>
-      <p>Leonardo Guillermo Falcón Choque - 20190702</p>
-      <p>Juan Diego Rojas</p>
-
-    </div>
-  </div>
-          
-      </footer>
+      </main>  
     </div>
   );
 }
