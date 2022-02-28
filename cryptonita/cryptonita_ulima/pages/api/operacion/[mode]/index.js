d9af3b448b4433a62ad9c414b1962f161ff77d1a
DogeFox071101 <lgfalconch@hotmail.com>
2022-02-27 21:55:21 -0500

    Migrates Ajustados

---
+++ cryptonita/cryptonita_ulima/pages/api/operacion/[mode]/index.js
@@ -0,0 +1,45 @@
+import { genOperacion, getOperacion, getOperaciones, getOperacionesUsuario, setOperacion, delOperacion } from "../../../dao/operacion";
+
+const operacionHandler = async (req, res) => {
+    if (req.method == "GET") {
+        const operaciones = await getOperaciones()
+        const operacionesEnLista = []
+        for (let operacion of operaciones) {
+            operacionesEnLista.push({
+                id : operacion.id,
+                tipoCambio : operacion.tipoCambio,
+                montoBtc : operacion.montoBtc,
+                tipoOperacion : operacion.tipoOperacion,
+                estado : operacion.estado,
+                billeteraBtc : operacion.billeteraBt,
+                cuentaBancaria : operacion.cuentaBancaria,
+                codigoTransaccion : operacion.codigoTransaccion,
+                createdAt: operacion.createdAt,
+                updatedAt: operacion.updatedAt
+            })
+        }
+        res.json({
+            msg : "",
+            proyectos : operacionesEnLista
+        })
+    }
+    else if (req.method == "POST") {
+        const data = JSON.parse(req.body)
+        await genOperacion(data.tipoCambio, data.montoBtc, data.tipoOperacion, data.idCliente)
+        res.json({
+            msg : ""
+        })
+    }
+    else if (req.method == "PUT") {
+        const data = JSON.parse(req.body)
+        await setOperacion(data)
+        res.json({
+            msg : ""
+        })
+    }
+    else {
+        res.status(400).json({
+            msg : "Método no definido"
+        })
+    }
+}
--- cryptonita/cryptonita_ulima/sequelize/migrations/20220227052332-correccion_tipoCambio2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('TipoCambio');
-  }
-};
