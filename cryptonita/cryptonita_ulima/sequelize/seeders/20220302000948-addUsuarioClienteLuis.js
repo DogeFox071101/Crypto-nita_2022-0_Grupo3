0f1f5e138ab4b8285eccbfbc18772310f17c3161
LuisMondonedo <lm.mc.1999@gmail.com>
2022-03-01 19:15:40 -0500

    Seed Usuario
    Se agrego un usuario para ingresar como cliente

---
+++ cryptonita/cryptonita_ulima/sequelize/seeders/20220302000948-addUsuarioClienteLuis.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert("Usuario",[
+      {
+        nombre : "Luis Manuel",
+        apellidos : "Mondoñedo Cabanillas",
+        dni : "70412766",
+        email : "cliente@gmail.com",
+        password : "3992792b4a8365953c5dd32122599864e5eaa858f1d631f04e6d51ee86c48031",
+        telefono : "955758929",        
+        createdAt : new Date(),
+        updatedAt : new Date()
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
