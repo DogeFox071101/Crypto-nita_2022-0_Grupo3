efc961811795f7aa3040fcf352fbb63e0d2c0f12
LuisMondonedo <lm.mc.1999@gmail.com>
2022-03-03 15:00:42 -0500

    CompraBtcSeg
    Se implemento la interfaz compraBtcTer

--- cryptonita/cryptonita_ulima/pages/compraBtcSeg.js
+++ cryptonita/cryptonita_ulima/pages/compraBtcSeg.js
@@ -1,11 +1,60 @@
 import Header from "../components/header.component"
 import Footer from "../components/footer.component"
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const CompraBtcSeg = () => {
+    const [txtCuentaBancaria, setTxtCuentaBancaria] = useState("")
+    const [txtCodigoTransaccion, setTxtCodigoTransaccion] = useState("")
+    const [operacion, setOperacion] = useState(null)
+
+    const txtCodigoTransaccionOnChange = (event) => {
+        const txtCodigoTransaccionIngresado = event.target.value
+        setTxtCodigoTransaccion(txtCodigoTransaccionIngresado)
+    }
+
+    const txtCuentaBancariaOnChange = (event) => {
+        const txtCuentaBancariaIngresado = event.target.value
+        setTxtCuentaBancaria(txtCuentaBancariaIngresado)
+    }
+
+    const obtenerOperacionIncompleta = async () => {
+        const resp = await fetch(`/api/operacion/incomplete/${localStorage.getItem("idCliente")}`)
+        const data = await resp.json()
+        setOperacion(data.operacion)
+        console.log(data.operacion)
+    }
+
+    const actualizarOperacionCompraDos = () => {
+        const operacionActual = operacion
+        operacionActual.codigoTransaccion = txtCodigoTransaccion
+        operacionActual.cuentaBancaria = txtCuentaBancaria
+        operacionActual.estado = "Pendiente"
+        setOperacion(operacionActual)
+    }
+
+    const subirOperacionPendiente = async () => {
+        localStorage.setItem("ultimaOperacion", operacion.id)
+        const resp = await fetch("/api/operacion", {
+            method : "PUT",
+            body : JSON.stringify(operacion)
+        })
+        const data = await resp.json()
+    }
+
+    const buttonNextHandler = async () => {
+        actualizarOperacionCompraDos()
+        await subirOperacionPendiente()
+        location.href = "/compraBtcTer"
+    }
+
+    const cancelOp = () => {
+        location.href = "/compraVenta"
+    }
+
     useEffect(() => {
         document.title = "Paso 2 de 3 | Compra de BTC"
-    })
+        obtenerOperacionIncompleta()
+    },[])
     return <div>
         <Header></Header>
         <main className="p-2 mt-4">
@@ -25,18 +74,18 @@ const CompraBtcSeg = () => {
                     <div className="px-0">
                         <label className="form-label" htmlFor="">(1) Número de cuenta bancaria: </label>
                     </div>
-                    <input className="form-control" type="text" placeholder="Ejemplo: 192-11303551-0-27" />
+                        <input className="form-control" type="text" defaultValue={ txtCuentaBancaria } onChange={ txtCuentaBancariaOnChange } />
                     <div className="px-0 mt-4">
                         <label className="form-label" htmlFor="">(2) Ingresa aquí el código de transacción:</label>
                     </div>
-                    <input className="form-control mb-4" type="text" placeholder="Ejemplo: 564154d5we4f5egsefw"/>
+                        <input className="form-control mb-4" type="text" defaultValue={ txtCodigoTransaccion } onChange={ txtCodigoTransaccionOnChange } />
                 </div>
                 <div className=" row col-5 mx-auto mb-5">
                     <div className="col-6 pe-1 ps-0">
-                        <button className="btn btn-success col-12">Regresar</button>
+                        <button className="btn btn-success col-12" onClick={ cancelOp }>Cancelar</button>
                     </div>
                     <div className="col-6 ps-1 pe-0">
-                        <button className="btn btn-success col-12">Continuar</button>
+                        <button className="btn btn-success col-12" onClick={ buttonNextHandler }>Continuar</button>
                     </div>
                 </div>
             </div>
